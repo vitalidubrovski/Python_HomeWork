e66d75d76e1c9403f89f1ec41b93435a3ce604a0
Noah <vik_026@mail.ru>
2023-01-05 22:16:52 +0300

    home_work_08_candy_bot

--- Hw_09/main.py
+++ Hw_09/main.py
@@ -10,3 +10,4 @@ if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
 
 # homework_candy_bot название бота, ключ оставил внутри.
+# 5989854134:AAFM7UAUdJsBNLfXkuw4vN4wS_U3CzSKkjg
